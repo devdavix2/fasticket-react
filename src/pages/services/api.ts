b3088447a129449d17ef1a1ce0c1d628d445c96a
devdavix2 <devdavixx@gmail.com>
2025-01-11 23:46:14 +0100

    add

--- src/pages/services/api.ts
+++ src/pages/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8000";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
 
 
 // Helper function to extract and handle error messages
